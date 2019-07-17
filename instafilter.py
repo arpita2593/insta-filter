69036496cf321606b8dcf0d1068596f2afda8c13
arpita2593 <arpitakulkarni08@gmail.com>
2019-07-17 20:10:12 +0530

    Removed third image

--- instafilter.py
+++ instafilter.py
@@ -1,18 +1,19 @@
+#import image from PIL
 from PIL import Image
-
+#load images into objects
 base_img = Image.open("2.jpg")
 img_filter = Image.open("filter5.jpg")
 third = Image.open("1.jpg")
-
+#set o\p image size
 size = (760,760)
-
+#resize all images
 base_img=base_img.resize(size)
 img_filter=img_filter.resize(size)
-third = third.resize(size)
 
+#split each image
 r,g,b = base_img.split()
 R,G,B = img_filter.split()
-Rr,Gg,Bb = third.split()
 
+#merge
 im = Image.merge("RGB",(R,g,B))
 im.show()
